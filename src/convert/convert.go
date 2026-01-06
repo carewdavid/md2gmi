cd9cafc6d50b8f6345b05c8d96467c9b3c0807a8
David Carew <david@dcarew.com>
2026-01-06 11:28:05 -0800

    Format links for output

--- src/convert/convert.go
+++ src/convert/convert.go
@@ -1,5 +1,14 @@
 package convert
 
+import (
+	"fmt"
+	"net/url"
+)
+
 func Convert(markdown string) string {
-	return ""
+	return markdown
+}
+
+func FormatLink(linkTarget *url.URL, linkText string) string {
+	return fmt.Sprintf("=> %v %v\n", linkTarget.String(), linkText)
 }
