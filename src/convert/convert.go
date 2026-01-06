72cff1c980a2f4f5b2a782806398d856ccf74e25
David Carew <david@dcarew.com>
2026-01-06 12:00:48 -0800

    Convert input linewise

--- src/convert/convert.go
+++ src/convert/convert.go
@@ -1,14 +1,29 @@
 package convert
 
 import (
+	"bufio"
 	"fmt"
 	"net/url"
+	"strings"
 )
 
 func Convert(markdown string) string {
+	scanner := bufio.NewScanner(strings.NewReader(markdown))
+	output := strings.Builder{}
+	//Assume one paragraph per line
+	//TODO: more sophisticated chunking of input text
+	for scanner.Scan() {
+		output.WriteString(ConvertLine(scanner.Text()))
+	}
+	return output.String()
+}
+
+func ConvertLine(markdown string) string {
 	return markdown
 }
 
+// Format linkTarget and linkText as a gemtext link. The format is extremely simple:
+// => gemini://example.com/ Example Text
 func FormatLink(linkTarget *url.URL, linkText string) string {
 	return fmt.Sprintf("=> %v %v\n", linkTarget.String(), linkText)
 }
