59754d7d8b9ffa421674fd57a8ecb891f34ed1c1
David Carew <david@dcarew.com>
2026-01-18 09:13:17 -0800

    Adjust tests for ConvertLine to refine expected behavior

--- src/convert/convert_test.go
+++ src/convert/convert_test.go
@@ -9,11 +9,11 @@ func TestConvertLine(t *testing.T) {
 	}{
 		{"", ""},
 		{"The quick brown fox jumps over the lazy dog.", "The quick brown fox jumps over the lazy dog."},
-		{"<https://example.com/>", "=> https://example.com/\n"},
-		{"[This is a test link.](https://example.com)", "=> https://example.com This is a test link.\n"},
-		{"[Exemplar on Wikipedia](https://en.wikipedia.org/wiki/Exemplar_(disambiguation)", "=> https://en.wikipedia.org/wiki/Exemplar_(disambiguation) Exemplar on Wikipedia\n"},
-		{"Test with [square brackets] but no link.", "Test with [square brackets] but no link.\n"},
-		{"This is a paragraph containing a [link](https://example.com) to somewhere else.", "This is a paragraph containing a link[*] to somewhere else.\n=> https://example.com link\n"},
+		{"<https://example.com/>", "=> https://example.com/"},
+		{"[This is a test link.](https://example.com)", "=> https://example.com This is a test link."},
+		{"[Exemplar on Wikipedia](https://en.wikipedia.org/wiki/Exemplar_(disambiguation)", "=> https://en.wikipedia.org/wiki/Exemplar_(disambiguation) Exemplar on Wikipedia"},
+		{"Test with [square brackets] but no link.", "Test with [square brackets] but no link."},
+		{"This is a paragraph containing a [link](https://example.com) to somewhere else.", "This is a paragraph containing a link[*] to somewhere else.\n=> https://example.com link"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.in, func(t *testing.T) {
